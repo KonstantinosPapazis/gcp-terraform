1866a6e018d5751652606ca4372cacc15f24a787
Konstantinos Papazis <kpapazis@konstantinoss-macbook-pro-1.home>
2022-11-01 15:40:53 +0100

    add bucket file

---
+++ bucket.tf
@@ -0,0 +1,43 @@
+resource "google_storage_bucket" "bucket1" {
+  name     = "test-bucket-abc"
+  location = "EUROPE-WEST1"
+}
+
+resource "google_service_account" "custom-disk" {
+  account_id   = "custom-service-account-bucket"
+  display_name = "A custom testservice account for bucket"
+}
+
+#One way to assign iam to a bucket
+
+#resource "google_storage_bucket_iam_binding" "binding" {
+#  bucket = google_storage_bucket.bucket1.name
+#  role = "roles/storage.admin"
+#  members = [
+#    "serviceAccount:${google_service_account.custom-disk.email}",
+#    "user:kpapazis84@gmail.com",
+#  ]
+#}
+
+#Second way to assign iam to the bucket 
+
+#resource "google_storage_bucket_iam_member" "member" {
+#  bucket = google_storage_bucket.bucket1.name
+#  role = "roles/storage.admin"
+#  member = "user:kpapazis84@gmail.com"
+#}
+
+#More advanced scenario
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/storage.admin"
+    members = [
+      "user:kpapazis84@gmail.com",
+    ]
+  }
+}
+
+resource "google_storage_bucket_iam_policy" "policy" {
+  bucket = google_storage_bucket.bucket1.name
+  policy_data = data.google_iam_policy.admin.policy_data
+}
